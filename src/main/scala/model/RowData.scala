45486c1c9cc396bf32db22384bdd75d3931b655a
Michał <udtt.kuzma@gmail.com>
2020-07-14 16:13:22 +0200

    Closes #4 Implement tree building

--- src/main/scala/model/RowData.scala
+++ src/main/scala/model/RowData.scala
@@ -1,3 +1,7 @@
 package model
 
-final case class RowData(name: String, id: Int)
+final case class RowData(name: String, id: Int) {
+  def containsChildrenOf(parentName: String): Boolean = {
+    name.matches(parentName + ".")
+  }
+}
