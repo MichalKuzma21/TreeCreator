e095dc8ed7f5d99b55dbd53eb3585021f0b7c627
Michał <udtt.kuzma@gmail.com>
2020-07-14 13:50:20 +0200

    Closes #2 Implement JSON deserialization

--- src/main/scala/model/Node.scala
+++ src/main/scala/model/Node.scala
@@ -1,3 +1,11 @@
 package model
 
+import io.circe.Encoder
+import io.circe.generic.semiauto.deriveEncoder
+
 final case class Node(id: Int, name: String, nodes: List[Node])
+
+object Node {
+  implicit val nodeEncoder: Encoder[Node] = deriveEncoder
+}
+
